581d503e01483748cc296964b76461027f03a03b
Tao Huang <1392631950@qq.com>
2021-09-29 20:40:58 +0800

    9.29晚上8：40

--- data_process.py
+++ data_process.py
@@ -6,24 +6,34 @@ import pandas as pd
 import numpy as np
 from scipy.io import arff
 
-train_file_path = 'yeast_corpus/yeast-train.arff'
+train_file_path = 'yeast_corpus/yeast-train.arff'#获取训练集路径
 test_file_path = 'yeast_corpus/yeast-test.arff'
 
-train_data, meta = arff.loadarff(train_file_path)
+train_data, meta = arff.loadarff(train_file_path)  #读取训练集数据,返回的第一个数据是array，第二个数据是其他的文件信息
 test_data, meta = arff.loadarff(test_file_path)
+#print('train_data:','\n',train_data )
 
-train_df = pd.DataFrame(train_data)
+train_df = pd.DataFrame(train_data)       #因为array操作并不方便，所以转换为df
 test_df = pd.DataFrame(test_data)
+#print('train_df:','\n',train_df )
 
-train_data = train_df.values
+train_data = train_df.values                #values为ndarray
 test_data = test_df.values
+#print("tran_data:",'\n',train_data )
 
-train_x = np.array(train_data[:, 0:103])
+
+train_x = np.array(train_data[:, 0:103])     #所有行、第1-103列,需要自己去数多少行
 train_y = np.array(train_data[:, 104:117])
 test_x = np.array(test_data[:, 0:103])
 test_y = np.array(test_data[:, 104:117])
 
-np.save('dataset/train_x.npy', train_x)
-np.save('dataset/train_y.npy', train_y)
-np.save('dataset/test_x.npy', test_x)
-np.save('dataset/test_y.npy', test_y)
+def save(path,data):
+    df=pd.DataFrame(data)
+    df.to_excel(path)
+
+#save('dataset/train_x.xlsx', train_x)
+np.savetxt('dataset/train_x1.txt', train_x)
+#save('dataset/test_x.xlsx', test_x)
+np.savetxt('dataset/test_x1.txt', test_x)
+##上次把dataset中的数据用xlsx存贮了
+##下次应该把剩余的数据也用xlsx存储
--- mlknn.py
+++ mlknn.py
@@ -1,175 +1,247 @@
 #@Time      :2018/9/14 14:27
 #@Author    :zhounan
 # @FileName: mlknn.py
-
 import numpy as np
+import pandas as pd
+'''
+#######################################################################################没问题
+def dis(nda1,nda2):##计算两个属性之间的距离
+    nda1=set(nda1);nda2 =set(nda2)
+    if -1.0 in nda2:
+        nda2.remove(-1)
+    if -1.0 in nda1:
+        nda1.remove(-1)
+    return len(nda1^nda2 )
 
-# find k neighbors
-def knn(train_x, t_index, k):
-    data_num = train_x.shape[0]
-    dis = np.zeros(data_num)
-    neighbors = np.zeros(k)
+def knn(train_x, t_index, k):##寻找离第t个最近的k个元素的序号
+    data_num = train_x.shape[0]##第一个维度的个数
+    neighbors = np.zeros(k)##类别个数
 
-    for i in range(data_num):
-        dis[i] = ((train_x[i] - train_x[t_index]) ** 2).sum()
-
+    dist = [dis(train_x [i],train_x[t_index ]) for i in range(data_num) ]
+    dist[t_index ]=float('inf')
     for i in range(k):
-
         temp = float('inf')
         temp_j = 0
         for j in range(data_num):
-            if (j != t_index) and (dis[j] < temp):
-                temp = dis[j]
-                temp_j = j
-        dis[temp_j] = float('inf')
+            if (j != t_index) and (dist[j] < temp):
+                temp = dist[j]
+                temp_j = j##最后的temp是最短距离,temp_j是最短距离的点的序号
+        dist[temp_j] = float('inf')##该点已经被加进neighbors了，所以不能是最短的了，即变成了inf
         neighbors[i] = temp_j
 
     return neighbors
 
-
-def knn_test(train_x, t, k):
-    data_num = train_x.shape[0]
-    dis = np.zeros(data_num)
-    neighbors = np.zeros(k)
-
-    for i in range(data_num):
-        dis[i] = ((train_x[i] - t) ** 2).sum()
-
-    for i in range(k):
-
-        temp = float('inf')
-        temp_j = 0
-        for j in range(data_num):
-            if dis[j] < temp:
-                temp = dis[j]
-                temp_j = j
-        dis[temp_j] = float('inf')
-        neighbors[i] = temp_j
-
-    return neighbors
-
-def evaluation():
-    test_y = np.load('dataset/test_y.npy')
-    predict = np.load('parameter_data/predict.npy')
+def evaluation(test_y,predict):##评估模型好坏，数据的导入与输出
     test_y = test_y.astype(np.int)
-
+    print('------','test_y','------','\n',test_y)
+    dfy=pd.DataFrame (test_y )
+    dfpre=pd.DataFrame (predict )
+    dfy.to_csv(r"C:/Users/86159/PycharmProjects/MLKNN/data/test_y.csv")
+    dfpre.to_csv(r"C:/Users/86159/PycharmProjects/MLKNN/data/predict.csv")
+    print('\n')
+    print('------','predict','------','\n',predict )
     hamming_loss = HammingLoss(test_y, predict)
     print('hamming_loss = ', hamming_loss)
 
-def HammingLoss(test_y, predict):
+def HammingLoss(test_y, predict):##
     label_num = test_y.shape[1]
     test_data_num = test_y.shape[0]
     hamming_loss = 0
     temp = 0
     for i in range(test_data_num):
-        temp = temp + np.sum(test_y[i] ^ predict[i])
+        ss=np.sum(test_y[i] ^ predict[i])
+        temp = temp +ss#按位异或运算符，a=60=0011 1100 b=13=0000 1101 → a^b=0011 0001=49，因为数据只有0和1，所以可以表示对称差,
 
     hamming_loss = temp / label_num / test_data_num
 
     return hamming_loss
 
-
-class MLKNN(object):
-    s = 1
-    k = 10
-    label_num = 0
-    train_data_num = 0
-    train_x = np.array([])
-    train_y = np.array([])
-
-    Ph1 = np.array([])
-    Ph0 = np.array([])
-    Peh1 = np.array([])
-    Peh0 = np.array([])
-
-    def __init__(self, train_x, train_y, k ,s):
-        self.k = k
-        self.s = s
-        self.train_x = train_x
-        self.train_y = train_y
-        self.label_num = train_y.shape[1]
-        self.train_data_num = train_x.shape[0]
-        self.Ph1 = np.zeros(self.label_num)
-        self.Ph0 = np.zeros(self.label_num)
-        self.Peh1 = np.zeros([self.label_num, self.k + 1])
-        self.Peh0 = np.zeros([self.label_num, self.k + 1])
-
-    def train(self):
-        #computing the prior probabilities
-        for i in range(self.label_num):
-            cnt = 0
-            for j in range(self.train_data_num):
-                if self.train_y[j][i] == 1:
-                    cnt = cnt + 1
-            self.Ph1[i] = (self.s + cnt) / (self.s * 2 + self.train_data_num)
-            self.Ph0[i] = 1 - self.Ph1[i]
-
-        for i in range(self.label_num):
-
-            print('training for label\n', i + 1)
-            c1 = np.zeros(self.k + 1)
-            c0 = np.zeros(self.k + 1)
-
-            for j in range(self.train_data_num):
-                temp = 0
-                neighbors = knn(self.train_x, j, self.k)
-
-                for k in range(self.k):
-                    temp = temp + int(self.train_y[int(neighbors[k])][i])
-
-                if self.train_y[j][i] == 1:
-                    c1[temp] = c1[temp] + 1
-                else:
-                    c0[temp] = c0[temp] + 1
-
-            for j in range(self.k + 1):
-                self.Peh1 = (self.s + c1[j]) / (self.s * (self.k + 1) + np.sum(c1))
-                self.Peh0 = (self.s + c0[j]) / (self.s * (self.k + 1) + np.sum(c0))
+#######读取文件
+train_x = np.loadtxt('dataset/train_yasuo_x.txt')
+print('------','train_x1','------','\n',train_x )
+#print(type(train_x ))
+train_y = np.loadtxt('dataset/train_yasuo_y.txt')
+print('------','train_y','------','\n',train_y )
 
 
-    def save(self):
-        np.save('parameter_data/Ph1.npy', self.Ph1)
-        np.save('parameter_data/Ph0.npy', self.Ph0)
-        np.save('parameter_data/Peh1.npy', self.Peh1)
-        np.save('parameter_data/Peh0.npy', self.Peh0)
+#######初始化
+k = 10
+s = 1
+label_num = train_y.shape[1]##第二行的列数
+train_data_num = train_x.shape[0]
+Ph1 = np.zeros(label_num)
+Ph0 = np.zeros(label_num)
+Peh1 = np.zeros([label_num, k + 1])#+1:0个，1个，，，，，k个，所以要加一
+Peh0 = np.zeros([label_num, k + 1])
 
-    def load(self):
-        Ph1 = np.load('parameter_data/Ph1.npy')
-        Ph0 = np.load('parameter_data/Ph0.npy')
-        Peh1 = np.load('parameter_data/Peh1.npy')
-        Peh0 = np.load('parameter_data/Peh0.npy')
 
-    def test(self):
-        test_x = np.load('dataset/test_x.npy')
-        test_y = np.load('dataset/test_y.npy')
-        predict = np.zeros(test_y.shape, dtype=np.int)
-        test_data_num = test_x.shape[0]
+########
+#计算先验概率duiduiduiduiduiduidui
+for i in range(label_num):  ##对每一个标签而言
+    cnt = 0  ##计数参量
+    for j in range(train_data_num):  ##对于每一个示例而言
+        if train_y[j][i] == 1:  ##第j个示例有第i个标签########需要改!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            cnt = cnt + 1  ##循环结束之后是，有i标签的示例的个数
+    Ph1[i] = (s + cnt) / (s * 2 + train_data_num)  ##计算ph1，i.e.一个示例有i标签的概率
+    Ph0[i] = 1 - Ph1[i]  ##ph0，i.e.一个示例没有i标签的概率
 
-        for i in range(test_data_num):
-            neighbors = knn_test(self.train_x, test_x[i], k)
 
-            for j in range(self.label_num):
-                temp = 0
-                for nei in neighbors:
-                    temp = temp + int(self.train_y[int(nei)][j])
+for i in range(k):  ##对于每一个标签
+    print('training for label\n', i + 1)
+    c1 = np.zeros(k + 1)  ##c[]，初始化
+    c0 = np.zeros(k + 1)  ##c'[],初始化
+    for j in range(train_data_num):  ##对每一个示例
+        temp = 0
+        neighbors = knn(train_x, j, k)  ##找到该示例的邻集
 
-                if(self.Ph1[j] * self.Peh1[j][temp] > self.Ph0[j] * self.Peh0[j][temp]):
-                    predict[i][j] = 1
-                else:
-                    predict[i][j] = 0
+        for l in range(k):  ##对每一个邻居而言,为的是求k个邻居中的有几个有这个i标签
+            temp = temp + int(train_y[int(neighbors[l])][i])  ##循环结束之后temp是t的邻集中有i标签的个数
 
-        np.save('parameter_data/predict.npy', predict)
+        if train_y[j][i] == 1:  ##判断t有没有i标签
+            c1[temp] = c1[temp] + 1
+        else:
+            c0[temp] = c0[temp] + 1
 
-if __name__ == '__main__':
-    k = 10
-    s = 1
-    train_x = np.load('dataset/train_x.npy')
-    train_y = np.load('dataset/train_y.npy')
+    for j in range(k + 1):
+        Peh1[i][j] = (s + c1[j]) / (s * (k + 1) + np.sum(c1))  ##计算peh1，i.e.t有i标签的情况下，邻集有j个含有i标签的概率
+        Peh0[i][j] = (s + c0[j]) / (s * (k + 1) + np.sum(c0))
 
-    mlknn = MLKNN(train_x, train_y, k, s)
-    #mlknn.train()
-    #mlknn.save()
-    #mlknn.load()
-    #mlknn.test()
+test_x = np.loadtxt('dataset/test_yasuo_x.txt')
+test_y = np.loadtxt('dataset/test_yasuo_y.txt')
+predict = np.ones(test_y.shape, dtype=np.int)
+test_data_num = test_x.shape[0]##读取数据
 
-    evaluation()
+for i in range(test_data_num):##对于每个示例而言
+    neighbors = knn_test(train_x, test_x[i], k)##寻找第i个示例的最近的k个示例的index
+    for j in range(label_num):##对于每个标签而言
+        temp = 0
+        for nei in neighbors:##nei就是index
+            temp = temp + int(train_y[int(nei)][j])
+        if(Ph1[j] * Peh1[j][temp] > Ph0[j] * Peh0[j][temp]):##P(Elj,Hl1)>P(Elj,Hl0),i.e.示例有i，有j个邻居有i同时发生的概率,>,示例没有i，有j个邻居有i同时发生的概率
+            predict[i][j] = 1
+        else:
+            predict[i][j] = 0
+#np.save('parameter_data/predict.npy', predict,allow_pickle=True)
+print(predict )
+evaluation(test_y,predict)'''
+import numpy as np
+import pandas as pd
+
+#######################################################################################没问题
+def dis(nda1,nda2):##计算两个属性之间的距离
+    nda1=set(nda1);nda2 =set(nda2)
+    if -1.0 in nda2:
+        nda2.remove(-1)
+    if -1.0 in nda1:
+        nda1.remove(-1)
+    return len(nda1^nda2 )
+
+def knn(train_x, t_index, k):##寻找离第t个最近的k个元素的序号
+    data_num = train_x.shape[0]##第一个维度的个数
+    neighbors = np.zeros(k)##类别个数
+
+    dist = [dis(train_x [i],train_x[t_index ]) for i in range(data_num) ]
+    dist[t_index ]=float('inf')
+    for i in range(k):
+        temp = float('inf')
+        temp_j = 0
+        for j in range(data_num):
+            if (j != t_index) and (dist[j] < temp):
+                temp = dist[j]
+                temp_j = j##最后的temp是最短距离,temp_j是最短距离的点的序号
+        dist[temp_j] = float('inf')##该点已经被加进neighbors了，所以不能是最短的了，即变成了inf
+        neighbors[i] = temp_j
+
+    return neighbors
+
+def evaluation(test_y,predict):##评估模型好坏，数据的导入与输出
+    test_y = test_y.astype(np.int)
+    print('------','test_y','------','\n',test_y)
+    print('\n')
+    print('------','predict','------','\n',predict )
+    hamming_loss=0
+    for i in range(test_y .shape [1]):
+        hamming_loss += dis(test_y[i], predict[i])
+    hamming_loss=hamming_loss/test_y .shape [1]/len(test_y [0])
+    print('hamming_loss = ', hamming_loss)
+
+#######读取文件
+train_x = np.loadtxt('dataset/train_yasuo_x.txt')
+print('------','train_x','------','\n',train_x )
+#print(type(train_x ))
+train_y = np.loadtxt('dataset/train_yasuo_y.txt')
+print('------','train_y','------','\n',train_y )
+
+
+#######初始化
+k = 10
+s = 1
+label_num = train_y.shape[1]##第二行的列数
+train_data_num = train_x.shape[0]
+Ph1 = np.zeros(label_num)
+Ph0 = np.zeros(label_num)
+Peh1 = np.zeros([label_num, k + 1])#+1:0个，1个，，，，，k个，所以要加一
+Peh0 = np.zeros([label_num, k + 1])
+
+
+########
+#计算先验概率
+for i in range(label_num):  ##对每一个标签而言
+    cnt = 0  ##计数参量
+    for j in range(train_data_num):  ##对于每一个示例而言
+        if i in train_y[j]:  ##第j个示例有第i个标签,判断j示例有没有i标签
+            cnt = cnt + 1  ##循环结束之后是，有i标签的示例的个数
+    Ph1[i] = (s + cnt) / (s * 2 + train_data_num)  ##计算ph1，i.e.一个示例有i标签的概率
+    Ph0[i] = 1 - Ph1[i]  ##ph0，i.e.一个示例没有i标签的概率
+
+for i in range(label_num):  ##对于每一个标签
+    print('training for label\n', i + 1)
+    c1 = np.zeros(k + 1)  ##c[]，初始化
+    c0 = np.zeros(k + 1)  ##c'[],初始化
+
+    for j in range(train_data_num):  ##对每一个示例
+        temp = 0
+        neighbors = knn(train_x, j, k)  ##找到该示例的邻集
+        for l in range(k):  ##对每一个邻居而言,为的是求k个邻居中的有几个有这个i标签
+            if i in train_y[int(neighbors[l])]:
+                temp += 1  ##循环结束之后temp是t的邻集中有i标签的个数
+
+        if i in train_y[j]:  ##判断t有没有i标签
+            c1[temp] = c1[temp] + 1
+        else:
+            c0[temp] = c0[temp] + 1
+
+    for j in range(k + 1):
+        Peh1[i][j] = (s + c1[j]) / (s * (k + 1) + np.sum(c1))  ##计算peh1，i.e.t有i标签的情况下，邻集有j个含有i标签的概率
+        Peh0[i][j] = (s + c0[j]) / (s * (k + 1) + np.sum(c0))
+
+test_x = np.loadtxt('dataset/test_yasuo_x.txt')
+test_y = np.loadtxt('dataset/test_yasuo_y.txt')
+predict = [list(np.ones(test_y.shape[1],dtype=np.int))]
+
+test_data_num = test_x.shape[0]##读取数据
+for i in range(test_data_num):##对于每个示例而言
+    temp0=test_x[i]
+    temp0=np.append(temp0,-1)
+    train_x[0]=temp0
+    neighbors = knn(train_x, 0, k)##寻找第i个示例的最近的k个示例的index
+    for j in range(label_num):##对于每个标签而言
+        temp = 0
+        temp1=[]
+        c=[]
+        for nei in neighbors:##nei就是index
+            if j in train_y[int(nei)]:
+                temp =temp+ 1  ##循环结束之后temp是t的邻集中有j标签的个数
+        if(Ph1[j] * Peh1[j][temp] > Ph0[j] * Peh0[j][temp]):##P(Elj,Hl1)>P(Elj,Hl0),i.e.示例有i，有j个邻居有i同时发生的概率,>,示例没有i，有j个邻居有i同时发生的概率
+            temp1.append(j)
+    predict.append(temp1)
+
+predict =pd.DataFrame (predict[1:] )
+predict =predict .values
+#np.save('parameter_data/predict.npy', predict,allow_pickle=True)
+print(predict )
+dfy=pd.DataFrame (test_y )
+dfpre=pd.DataFrame (predict )
+dfy.to_csv(r"C:/Users/86159/PycharmProjects/MLKNN/data/test_y.csv")
+dfpre.to_csv(r"C:/Users/86159/PycharmProjects/MLKNN/data/predict.csv")
+evaluation(test_y,predict)
